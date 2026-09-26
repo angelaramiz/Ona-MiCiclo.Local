ca43f4e9a7e99419446b60227061acc8ffd875c9
Angel Ramirez <angel@example.com>
2026-09-26 06:13:34 -0600

    Auto-release: Version 1.0.10-phase1 (Build 50)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.ona.miciclo"
         minSdk = 26
         targetSdk = 34
-        versionCode = 49
-        versionName = "1.0.9-phase1"
+        versionCode = 50
+        versionName = "1.0.10-phase1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
